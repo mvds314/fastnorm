4a8ccd8a4fee302d2ad463f54eb03566e1fb4173
mvds314 <postaanmartin@gmail.com>
2024-04-27 17:29:41 +0200

    Vectorized version of bvnu

--- fastnorm/bvnorm.py
+++ fastnorm/bvnorm.py
@@ -74,6 +74,7 @@ def bivar_norm_cdf(x, rho=0):
     This function is based on the method described by
     Drezner, Z and G.O. Wesolowsky, (1989), On the computation of the bivariate normal inegral, Journal of Statist. Comput. Simul. 35, pp. 101-107.
     """
+    return bvnl(x.T[0], x.T[1], rho)
     if x.ndim == 1:
         return bvnl(x[0], x[1], rho)
     elif x.ndim == 2:
@@ -230,6 +231,118 @@ def bvnu(a, b, rho=0):
     return p
 
 
+def bvnu_vectorized(a, b, rho=0):
+    r"""
+    Evaluate bivariate cummulative standard normal distribution function
+
+    .. math::
+        \int_a^\infty \int_b^\infty \phi(x,y,\rho) dxdy
+
+    with correlation coefficient :math:`\rho`.
+
+    This function is based on the method described in [1].
+
+    References
+    ----------
+    .. [1] Drezner, Z and G.O. Wesolowsky, (1989), On the computation of the bivariate normal inegral, Journal of Statist. Comput. Simul. 35, pp. 101-107.
+    """
+    global _w_bvnu_1, _x_bvnu_1, _w_bvnu_2, _x_bvnu_2, _w_bvnu_3, _x_bvnu_3
+    a = np.asanyarray(a)
+    b = np.asanyarray(b)
+    assert a.ndim == b.ndim == 1
+    assert len(a) == len(b)
+    # If uncorrelated, return the product of the marginals
+    if rho == 0:
+        return sps.norm.cdf(-a) * sps.norm.cdf(-b)
+    # If a or b is infinite set appropriate values
+    p = np.full(len(a), np.nan)
+    neginf_a = np.isneginf(a)
+    neginf_b = np.isneginf(b)
+    posinf_a = np.isposinf(a)
+    posinf_b = np.isposinf(b)
+    p[posinf_a | posinf_b] = 0
+    p[neginf_a & neginf_b] = 1
+    if np.any(neginf_a & ~neginf_b):
+        p[neginf_a & ~neginf_b] = sps.norm.cdf(-b[neginf_a & ~neginf_b])
+    if np.any(~neginf_a & neginf_b):
+        p[~neginf_a & neginf_b] = sps.norm.cdf(-a[~neginf_a & neginf_b])
+    # Handle the rest
+    sel = ~(neginf_a | neginf_b | posinf_a | posinf_b)
+    if np.any(sel):
+        h = a[sel]
+        k = b[sel]
+        tp = 2 * np.pi
+        hk = h * k
+        bvn = np.zeros(len(h))
+        if abs(rho) < 0.3:  # Gauss Legendre points and weights, n =  6
+            w = _w_bvnu_1
+            x = _x_bvnu_1
+        elif abs(rho) < 0.75:  # Gauss Legendre points and weights, n = 12
+            w = _w_bvnu_2
+            x = _x_bvnu_2
+        else:  # Gauss Legendre points and weights, n = 20
+            w = _w_bvnu_3
+            x = _x_bvnu_3
+        if abs(rho) < 0.925:
+            hs = np.expand_dims((h * h + k * k) / 2, axis=1)
+            asr = math.asin(rho) / 2
+            sn = np.sin(asr * x)
+            bvn = np.add(np.outer(hk, sn), -hs)
+            bvn = np.divide(bvn, np.expand_dims(1 - sn**2, axis=0))
+            bvn = np.dot(np.exp(bvn), w)
+            bvn = bvn * asr / tp + sps.norm.cdf(-h) * sps.norm.cdf(-k)
+        else:
+            if rho < 0:
+                k = -k
+                hk = -hk
+            if abs(rho) < 1:
+                ass = 1 - rho**2
+                a = math.sqrt(ass)
+                bs = np.square(h - k)
+                asr = -(bs / ass + hk) / 2
+                c = (4 - hk) / 8
+                d = (12 - hk) / 80
+                # TODO: continue here with cases -> set up bvn
+                cond = asr > -100
+                if np.any(cond):
+                    bvn = np.where(
+                        cond,
+                        a
+                        * np.exp(asr)
+                        * (1 - c * (bs - ass) * (1 - d * bs) / 3 + c * d * np.square(ass)),
+                        bvn,
+                    )
+                cond = hk > -100
+                if np.any(cond):
+                    b = np.sqrt(bs)
+                    spp = math.sqrt(tp) * sps.norm.cdf(-b / a)
+                    bvn = bvn - np.exp(-hk / 2) * spp * b * (1 - c * bs * (1 - d * bs) / 3)
+                a = a / 2
+                xs = (a * x) ** 2
+                asr = -(np.outer(bs, 1 / xs) + np.expand_dims(hk, 1)) / 2
+                ix = asr > -100
+                # TODO: this is the difficult part
+                xs = np.where(asr > -100, xs, 0)
+                spp = 1 + np.multiply(xs, np.expand_dims(c, axis=1)) * (
+                    1 + 5 * np.multiply(xs, np.expand_dims(d, axis=1))
+                )
+                rs = np.sqrt(1 - xs)
+                ep = np.exp(-np.expand_dims(hk / 2, axis=1) * xs / np.square(1 + rs)) / rs
+                bvn = (a * np.dot(np.exp(asr) * (spp - ep), w) - bvn) / tp
+            if rho > 0:
+                bvn = bvn + sps.norm.cdf(-np.max([h, k], axis=0))
+            elif h >= k:
+                bvn = -bvn
+            else:
+                if h < 0:
+                    L = sps.norm.cdf(k) - sps.norm.cdf(h)
+                else:
+                    L = sps.norm.cdf(-h) - sps.norm.cdf(-k)
+                bvn = L - bvn
+        p[sel] = np.clip(bvn, 0, 1)
+    return p
+
+
 def bvnl(a, b, rho=0):
     r"""
     Evaluate bivariate cummulative standard normal distribution function
@@ -241,4 +354,7 @@ def bvnl(a, b, rho=0):
     """
     a = np.asanyarray(a)
     b = np.asanyarray(b)
-    return bvnu(-a, -b, rho)
+    if a.ndim == 0:
+        return bvnu(-a, -b, rho)
+    else:
+        return bvnu_vectorized(-a, -b, rho)
