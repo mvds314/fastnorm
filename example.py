95a9e62d71ad113eb8c8db92f3bf787c558a5c0f
Schans <rob6027@robeco.nl>
2024-04-27 20:47:26 +0200

    Example with 100 tries

--- example.py
+++ example.py
@@ -1,5 +1,6 @@
 import numpy as np
 import scipy.stats as sps
+
 import fastnorm as fn
 from fastnorm.util import Timer
 
@@ -8,30 +9,39 @@ try:
     import approxcdf as acdf
 
     has_approxcdf = True
-except:
+except Exception:
     has_approxcdf = False
+
+n_tries = 100
+print(f"Timing bivariate normal cdf with {n_tries} tries")
 
 rho = 0.5
 
 with Timer("Scipy", decimals=6):
-    sps.multivariate_normal.cdf([1, 1], mean=[0, 0], cov=[[1, rho], [rho, 1]])
+    for i in range(n_tries):
+        sps.multivariate_normal.cdf([1, 1], mean=[0, 0], cov=[[1, rho], [rho, 1]])
 
 with Timer("Fastnorm", decimals=6):
-    fn.bivar_norm_cdf([1, 1], rho)
+    for i in range(n_tries):
+        fn.bivar_norm_cdf([1, 1], rho)
 
 if has_approxcdf:
     with Timer("Approx_cdf", decimals=6):
-        acdf.bvn_cdf(1, 1, rho)
+        for i in range(n_tries):
+            acdf.bvn_cdf(1, 1, rho)
 
-x = np.random.randn(1000, 2)
+x = np.random.randn(10000, 2)
 rho = 0.99
 
 with Timer("Scipy", decimals=6):
-    sps.multivariate_normal.cdf(x, mean=[0, 0], cov=[[1, rho], [rho, 1]])
+    for i in range(n_tries):
+        sps.multivariate_normal.cdf(x, mean=[0, 0], cov=[[1, rho], [rho, 1]])
 
 with Timer("Fastnorm", decimals=6):
-    fn.bivar_norm_cdf(x, rho)
+    for i in range(n_tries):
+        fn.bivar_norm_cdf(x, rho)
 
 if has_approxcdf:
     with Timer("Approx_cdf", decimals=6):
-        [acdf.bvn_cdf(xx[0], xx[1], rho) for xx in x]
+        for i in range(n_tries):
+            [acdf.bvn_cdf(xx[0], xx[1], rho) for xx in x]
