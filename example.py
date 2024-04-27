5c3d30ece6d52791b40d60f3250616101db48c80
mvds314 <postaanmartin@gmail.com>
2024-04-27 14:25:35 +0200

    Example update

--- example.py
+++ example.py
@@ -1,3 +1,4 @@
+import numpy as np
 import scipy.stats as sps
 import fastnorm as fn
 from fastnorm.util import Timer
@@ -9,3 +10,12 @@ with Timer("Scipy", decimals=6):
 
 with Timer("Fastnorm", decimals=6):
     fn.bivar_norm_cdf([1, 1], rho)
+
+x = np.random.randn(1000, 2)
+rho = 0.99
+
+with Timer("Scipy", decimals=6):
+    sps.multivariate_normal.cdf(x, mean=[0, 0], cov=[[1, rho], [rho, 1]])
+
+with Timer("Fastnorm", decimals=6):
+    fn.bivar_norm_cdf(x, rho)
