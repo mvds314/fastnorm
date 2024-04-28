6ceee358b2e33432eae9f2d974db2e2293489db6
mvd314 <postaanmartin@gmail.com>
2024-04-28 12:59:48 +0200

    Document performance

--- example.py
+++ example.py
@@ -15,23 +15,22 @@ except Exception:
 n_tries = 100
 print(f"Timing bivariate normal cdf with {n_tries} tries")
 
-rho = 0.5
-
+rho = 0.95
+x = [1, 1]
 with Timer("Scipy", decimals=6):
     for i in range(n_tries):
-        sps.multivariate_normal.cdf([1, 1], mean=[0, 0], cov=[[1, rho], [rho, 1]])
+        sps.multivariate_normal.cdf(x, mean=[0, 0], cov=[[1, rho], [rho, 1]])
 
 with Timer("Fastnorm", decimals=6):
     for i in range(n_tries):
-        fn.bivar_norm_cdf([1, 1], rho)
+        fn.bivar_norm_cdf(x, rho)
 
 if has_approxcdf:
     with Timer("Approx_cdf", decimals=6):
         for i in range(n_tries):
-            acdf.bvn_cdf(1, 1, rho)
+            acdf.bvn_cdf(*x, rho)
 
 x = np.random.randn(10000, 2)
-rho = 0.99
 
 with Timer("Scipy", decimals=6):
     for i in range(n_tries):
